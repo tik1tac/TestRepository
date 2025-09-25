e8f11975e7d74a017e6272ab89294ce3fbd563f8
Yuri Oveshnikov <Yurass228@yandex.ru>
2025-09-25 12:18:40 +0300

    Create Test Project

---
+++ TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var calc = new Calculator();
+
+        Console.WriteLine($"Сумма: {calc.Add(3, 4)}");
+        Console.WriteLine($"Произведение: {calc.Multiply(5, 6)}");
+    }
+}
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+
+    public int Multiply(int a, int b) => a * b;
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿namespace Tests
+{
+    public class UnitTest1
+    {
+        private readonly Calculator calc = new Calculator();
+
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.Equal(5, calc.Add(2, 3));
+            Assert.Equal(0, calc.Add(-1, 1));
+        }
+
+        [Fact]
+        public void TestMultiply()
+        {
+            Assert.Equal(6, calc.Multiply(2, 3));
+            Assert.Equal(-5, calc.Multiply(-1, 5));
+        }
+    }
+}
